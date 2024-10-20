73908b3742d4c04ba2c480c585b7e7943eecb211
Paul McQuade <paulmcquad@gmail.com>
2024-10-20 21:59:01 +0100

    Code Cleanup
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- 2D_Shapes/main.go
+++ 2D_Shapes/main.go
@@ -2,27 +2,14 @@ package main
 
 // fmt package provides the function to print anything
 import (
-	"fmt"
 	"plane/plane"
 )
 
 func main() {
 
-	// declaring the floating variables using the var keyword for
-	// storing the radius of the circle also a variable area to store Area
 	var Radius float64
 	var radius float64
 
-	fmt.Println("Annulus Calculator.")
-
-	// initializing the radius of a circle
-	fmt.Printf("Outer Radius = ")
-	fmt.Scan(&Radius)
-
-	// initializing the radius of a circle
-	fmt.Printf("Inner Radius = ")
-	fmt.Scan(&radius)
-
-	// finding the Area of a Circle
+	// finding the Area of an Annulus
 	plane.Annulus(Radius, radius)
 }
--- 2D_Shapes/plane/plane.go
+++ 2D_Shapes/plane/plane.go
@@ -6,11 +6,20 @@ import (
 	"math"
 )
 
+// finding the Area of an Annulus
 func Annulus(Radius float64, radius float64) float64 {
-	// returning the area by applying the formula
-	var Area float64
 
-	Area = math.Pi * (math.Pow(Radius, 2) - math.Pow(radius, 2))
+	fmt.Println("Annulus Calculator.")
+
+	// initializing the radius of the outer circle
+	fmt.Printf("Outer Radius = ")
+	fmt.Scan(&Radius)
+
+	// initializing the radius of the inner circle
+	fmt.Printf("Inner Radius = ")
+	fmt.Scan(&radius)
+
+	var Area float64 = math.Pi * (math.Pow(Radius, 2) - math.Pow(radius, 2))
 
 	fmt.Println("Annulus Area: ", Area)
 	return 0
