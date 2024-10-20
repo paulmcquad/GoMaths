1810f3859cedb89af9f5ce6abbc508d3638e20a1
Paul McQuade <paulmcquad@gmail.com>
2024-10-20 19:26:58 +0100

    Geometry Plane
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ plane/annulus.go
@@ -0,0 +1,27 @@
+package main
+
+// fmt package provides the function to print anything
+import (
+	"fmt"
+)
+
+func main() {
+
+	// declaring the floating variables using the var keyword for
+	// storing the radius of the circle also a variable area to store Area
+	var Radius float64
+	var radius float64
+
+	fmt.Println("Annulus Calculator.")
+
+	// initializing the radius of a circle
+	fmt.Printf("Outer Radius = ")
+	fmt.Scan(&Radius)
+
+	// initializing the radius of a circle
+	fmt.Printf("Inner Radius = ")
+	fmt.Scan(&radius)
+
+	// finding the Area of a Circle
+	Annulus(Radius, radius)
+}
---
+++ plane/plane.go
@@ -0,0 +1,17 @@
+package main
+
+// fmt package provides the function to print anything
+import (
+	"fmt"
+	"math"
+)
+
+func Annulus(Radius float64, radius float64) float64 {
+	// returning the area by applying the formula
+	var Area float64
+
+	Area = math.Pi * (math.Pow(Radius, 2) - math.Pow(radius, 2))
+
+	fmt.Println("Annulus Area: ", Area)
+	return 0
+}
