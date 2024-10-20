83a187029aeda2109e962a475f8ec5bcf8c125be
Paul McQuade <paulmcquad@gmail.com>
2024-10-20 22:30:53 +0100

    Plane.go
    Move some functions to plane.go
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- 2D_Shapes/main.go
+++ 2D_Shapes/main.go
@@ -1,15 +1,10 @@
 package main
 
-// fmt package provides the function to print anything
 import (
 	"plane/plane"
 )
 
 func main() {
 
-	var Radius float64
-	var radius float64
-
-	// finding the Area of an Annulus
-	plane.Annulus(Radius, radius)
+	plane.Menu()
 }
--- 2D_Shapes/plane/plane.go
+++ 2D_Shapes/plane/plane.go
@@ -6,6 +6,32 @@ import (
 	"math"
 )
 
+// Plane - Menu
+func Menu() {
+	var Radius float64
+	var radius float64
+	var menu int64
+
+	fmt.Println("2D Shapes:")
+	fmt.Println("1 - Area of a Circle")
+	fmt.Println("2 - Area of an Annulus")
+	fmt.Println("3 - Circumference of a Circle")
+
+	fmt.Scan(&menu)
+
+	switch menu {
+	case 1:
+		AreaOfCircle(radius)
+		break
+	case 2:
+		Annulus(Radius, radius)
+		break
+	case 3:
+		CircumferenceOfCircle(radius)
+		break
+	}
+}
+
 // finding the Area of an Annulus
 func Annulus(Radius float64, radius float64) float64 {
 
@@ -22,5 +48,36 @@ func Annulus(Radius float64, radius float64) float64 {
 	var Area float64 = math.Pi * (math.Pow(Radius, 2) - math.Pow(radius, 2))
 
 	fmt.Println("Annulus Area: ", Area)
+	return 0
+}
+
+// Find the Area of a Circle
+func AreaOfCircle(radius float64) float64 {
+
+	fmt.Println("Program to find the Area of a Circle.")
+
+	// initializing the radius of a circle
+	fmt.Printf("Radius = ")
+	fmt.Scan(&radius)
+
+	// returning the area by applying the formula
+	var Area float64 = math.Pi * (radius * radius)
+	fmt.Println("The Area of the circle =", Area, "cm^2")
+
+	return 0
+}
+
+// Find the Circumference of a Circle
+func CircumferenceOfCircle(radius float64) float64 {
+
+	fmt.Println("Program to find the Circumference of a Circle.")
+
+	// initializing the radius of a circle
+	fmt.Printf("Radius = ")
+	fmt.Scan(&radius)
+
+	var Circumference float64 = 2 * math.Pi * radius
+
+	fmt.Println("The Circumference of the circle =", Circumference, "cm^2")
 	return 0
 }
